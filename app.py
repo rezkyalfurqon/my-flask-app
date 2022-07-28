6f0020b1edaa27b46afb79c91213612bcc1e06dc
ilham-ma <ilhamaulana13@gmail.com>
2022-07-28 21:33:03 +0700

    create all algoritm

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def get_db():
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = training()
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
