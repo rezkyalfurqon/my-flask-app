357cca7790b00562aeb37cf7caa46a5db90939d0
ilham-ma <ilhamaulana13@gmail.com>
2022-08-04 22:44:37 +0700

    clear project

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import json
 import datetime
-from multiprocessing import dummy
 from flask import Flask, request
 from flask_cors import CORS
 from antares_http import antares
+from firebase import firebase_create
 
 # my module
 from machine_learning.training import training
 from machine_learning.model import model_nb, model_rf, model_svm, predict
 from dummy_sensor import get_data_dummy   
-from database.crud import get_data, add_data
-from firebase import firebase_create
 
 projectName = 'cobamqtt'
 deviceName = 'coba2'
@@ -52,13 +50,15 @@ def get_updates():
 async def monitor():
     try:
         response = json.loads(request.data)
+        # result = response['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:cin']['con']
         result = response['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:cin']['con']
         res = json.loads(result)
 
         sensor10 = res['Sensor10']
         dummy_data = get_data_dummy(sensor10)
+        convert_data = {}
 
-        convert_data = []
+        predict_data = []
         for data in dummy_data:
             level = 0
             if dummy_data[data] > 0 and dummy_data[data] < 90:
@@ -66,44 +66,40 @@ async def monitor():
             elif dummy_data[data] >= 90 :
                 level = 2
             
-            dummy_data[data] = level
-            convert_data.append(level)
+            convert_data[data] = level
+            predict_data.append(level)
 
 
-        kondisi_nb = int(predict(convert_data, 'nb'))
-        kondisi_rf = int(predict(convert_data, 'rf'))
-        kondisi_svm = int(predict(convert_data, 'svm'))
+        hasil_nb = int(predict(predict_data, 'nb'))
+        hasil_rf = int(predict(predict_data, 'rf'))
+        hasil_svm = int(predict(predict_data, 'svm'))
 
-        local_time = str(datetime.datetime.now().strftime("%H %M %S"))
+        local_time = str(datetime.datetime.now().strftime("%H:%M:%S"))
 
         antares_data = {
             "dummy_data": dummy_data,
             "convert_data": convert_data,
+            "local_time": local_time,
             "status": {
-                "naive_bayes": kondisi_nb,
-                "random_forest": kondisi_rf,
-                "support_vector_machine": kondisi_svm
-            },
-            "time": local_time
+                "naive_bayes": hasil_nb,
+                "random_forest": hasil_rf,
+                "support_vector_machine": hasil_svm
+            }
         }
 
         antares.send(antares_data, projectName, deviceName)
 
 
+        # add_data(allData)
+        firebase_create(antares_data, 'antares')
+        firebase_create({
+                "naive_bayes": hasil_nb,
+                "random_forest": hasil_rf,
+                "support_vector_machine": hasil_svm,
+                "local_time": local_time,
+            }, 'status')
     finally:
-        firebase_create(antares_data, 'data')
         return 'ack'
-
-# route to get all data from sqlite
-@app.route('/get_db', methods=['GET'])
-def get_db():
-    res = get_data()
-
-    return res
-
-@app.route('/tes', methods=['GET'])
-def tes():
-    return str(datetime.datetime.now().strftime("%H"))
 
 if __name__ == "__main__":
     X_train, X_test, y_train, y_test = training()
