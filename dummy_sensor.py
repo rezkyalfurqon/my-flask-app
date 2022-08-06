62faa4bd146b7e849b2ef679ee31ddca2c739760
rezkyalfurqon <rezkyalfurqon.ra7@gmail.com>
2022-08-06 20:31:02 +0700

    history add

--- dummy_sensor.py
+++ dummy_sensor.py
@@ -5,13 +5,13 @@ import random
 
 range_set = {
     'min' : [0, 0],
-    'max' : [80, 90]
+    'max' : [89, 90]
 }
 
 def sensor_generator():
     rand_num = random.randint(1, 101)
 
-    if rand_num <= 30 :
+    if rand_num <= 20 :
         return range_set['max']
     else :
         return range_set['min']
